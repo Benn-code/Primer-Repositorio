a18daf5eb0f20affc2c5eb318826ce50047f8af4
Benn <bsalgadoj@unal.edu.co>
2025-04-21 17:55:15 -0500

    Se elimina la captura de pantalla y se hacen modificaciones en guess-game.cpp

--- guess-game.cpp
+++ guess-game.cpp
@@ -6,22 +6,39 @@ Informar si lo adivinó o no
 */
 #include <iostream>
 
-int main(void) {
-    const int NUM = 10;
-    int guessed_number;
+// declaracion
+void play(void);
 
-    for( ; ; ) {
+int main(void) {
+    play();
+
+    return 0;
+}
+
+// implementación
+void play(void)
+{
+    const int NUM = 10;
+    int guessed_number = NUM/2;
+    const int MIN = 0;
+    const int MAX = 100;
+
+    while(guessed_number != NUM) {
     std::cout <<"Adivina un número entre 1 y 100:\n";
     std::cin >> guessed_number;
+    std::cout <<"Escribiste: " << guessed_number <<"\n";
 
-if (guessed_number == NUM) {
-    std::cout <<"Muy bien, ¡era ese!\n";
-    break;
-} else if (guessed_number > NUM) {
-    std::cout <<"Te pasaste\n";
-}    else { // es menor, no es necesario ponerlo, pues es la única posibilidad restante
-    std::cout <<"Te falta\n"; 
-}
+    //Validación del rango
+    if (guessed_number <= MIN or guessed_number > MAX){
+        std::cout <<"Eso nisiquiera está dentro del rango, adiós.\n";
+        break;
     }
-    return 0;
+    if (guessed_number == NUM) {
+        std::cout <<"Muy bien, ¡era ese!\n";
+     //Con el while, aquí habría el break en caso de acertar;
+    } else if (guessed_number > NUM) {
+    std::cout <<"Te pasaste\n";
+    }      else { // es menor, no es necesario ponerlo, pues es la única posibilidad restante
+    std::cout <<"Te falta\n"; }
+    }
 }
