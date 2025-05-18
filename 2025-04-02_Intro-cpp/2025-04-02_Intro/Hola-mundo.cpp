d7579ce5b2889fb97ca24f3ac8bc09d54a895544
Benn <bsalgadoj@unal.edu.co>
2025-05-18 02:11:17 -0500

    Se actualiza el primer programa con comentarios explicativos

--- 2025-04-02_Intro-cpp/2025-04-02_Intro/Hola-mundo.cpp
+++ 2025-04-02_Intro-cpp/2025-04-02_Intro/Hola-mundo.cpp
@@ -1,5 +1,17 @@
+/*Este es un primer proyecto de prueba que simplemente ejecuta la salida "¿Hola?"
+tal como tradicionalmente se hace en programación con "Hola mundo".
+El objetivo es verificar que el compilador y el entorno de desarrollo están correctamente configurados.
+*/
+
+//Para iniciar se añade una librería que incluye los comandos basicos de entrada y salida
 #include <iostream>
+
+/*Ahora se define la función principal del programa.
+Esta función se llama main.
+Aquí main no recibe argumentos ((void)) y devuelve un entero (int). 
+Luego, todo lo que esté entre corchetes ({}) es el cuerpo de la función.
+*/
 int main(void) {
-    std::cout <<"¿Hola?\n";
-    return 0;
+    std::cout <<"¿Hola?\n"; //Comando básico para imprimir en pantalla.
+    return 0; // La función main devuelve un entero, 0 para indicar que todo ha ido bien, cualquier otro valor para indicar un error.
 }
