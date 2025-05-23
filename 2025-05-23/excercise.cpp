f7b58d3d852d2539200ad4cdd46582d6ab0e1a06
Benn <bsalgadoj@unal.edu.co>
2025-05-23 08:02:34 -0500

    se añade primera parte

---
+++ 2025-05-23/excercise.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <numeric>
+
+int main(int argc , char **argv)
+{
+// Allocate memory for 10 integers
+int * dynamic_array = new int[5];
+// todo: check if memory was actually allocated
+
+// Use the array
+dynamic_array[0] = 2;
+dynamic_array[1] = 3;
+dynamic_array[2] = 4;
+dynamic_array[3] = 5;
+dynamic_array[4] = 6;
+//std::for_each(dynamic_array.begin(), dynamic_array.end(), print);
+
+//print
+
+std::cout << (&dynamic_array[0]) << "/n";
+
+// IMPORTANT: Must deallocate manually
+delete[] dynamic_array;
+}
